34cee3a32b8c0863614a6c98949e1130d26c64cc
bouni <bouni@owee.de>
2026-08-14 21:17:43 +0200

    minor improvements

--- src/fireplan/fireplan.py
+++ src/fireplan/fireplan.py
@@ -40,10 +40,17 @@ class Fireplan:
         return decorator
 
     def _handle_request(
-        self, method: str, endpoint: str, data: dict = {}, headers: dict = {}
+        self,
+        method: str,
+        endpoint: str,
+        data: dict | None = None,
+        headers: dict | None = None,
     ) -> dict | bool:
         """Send a post request to the Fireplan API."""
         url = f"{self.BASE_URL}{endpoint}"
+        data = data if data is not None else {}
+        headers = headers if headers is not None else {}
+
         try:
             response = requests.request(
                 method, url, headers={**self.headers, **headers}, timeout=5, json=data
@@ -60,8 +67,8 @@ class Fireplan:
         except requests.exceptions.RequestException as err:
             logger.error(f"Etwas ist schief gelaufen: {err}")
             return {}
+        self._log_result(endpoint, response)
         if response.ok:
-            self._log_result(endpoint, response)
             try:
                 return response.json()
             except JSONDecodeError:
@@ -71,7 +78,7 @@ class Fireplan:
 
     def _log_result(self, endpoint: str, response: requests.Response) -> None:
         """Print log message depending on response."""
-        if response:
+        if response.ok:
             logger.info(f"[{endpoint}] Senden erfolgreich")
             logger.info(f"[{endpoint}] Status code: {response.status_code}")
         else:
@@ -90,9 +97,6 @@ class Fireplan:
                     f"Validation error: {error['loc'][0]}, {error['msg']}, value was {error['input']}"
                 )
             return {}
-        if not any(data.values()):
-            logger.error("Keine nutzbaren Daten")
-            return {}
         return data
 
     def register(self, division: str) -> bool:
@@ -100,7 +104,7 @@ class Fireplan:
         data = self._handle_request(
             "GET", f"Register/{division}", headers={"API-Key": self._apikey}
         )
-        if not isinstance(data, bool):
+        if isinstance(data, dict) and data:
             self._apitoken = data["utoken"]
             self.headers["API-Token"] = self._apitoken
             return True
@@ -110,6 +114,8 @@ class Fireplan:
     def send_alarm(self, data: dict) -> dict | bool:
         """Send alarm data to the API."""
         data = self._validate(data, AlarmDataModel)
+        if not data:
+            return False
         return self._handle_request("POST", "Alarmierung", data)
 
     @_log_api_result("Einsatzliste")
@@ -125,24 +131,32 @@ class Fireplan:
             "Einsatztagebuch",
             headers={"EinsatzNrIntern": operation_number, "Standort": location},
         )
+        if not data:
+            return False
         return data
 
     @_log_api_result("Einsatztagebuch")
     def add_operations_log(self, data: dict) -> dict | bool:
         """Add an operation log."""
         data = self._validate(data, OperationDataModel)
+        if not data:
+            return False
         return self._handle_request("POST", "Einsatztagebuch", data)
 
     @_log_api_result("FMSStatus")
     def set_fms_status(self, data: dict) -> dict | bool:
         """Set the FMS status for a vehicle."""
         data = self._validate(data, FMSStatusDataModel)
+        if not data:
+            return False
         return self._handle_request("POST", "FMSStatus", data)
 
     @_log_api_result("Kalender")
     def get_calendar(self) -> dict | bool:
         """Get calendar from the API."""
         data = self._handle_request("GET", "Kalender")
+        if not data:
+            return False
         return data
 
     def send_inbound_sms(self, data: dict) -> bool:
@@ -154,16 +168,22 @@ class Fireplan:
     def get_other_services(self, year: int) -> dict | bool:
         """Get other services for a given year from the API."""
         data = self._handle_request("GET", f"SonstigeDienste/{year}")
+        if not data:
+            return False
         return data
 
     @_log_api_result("Termine")
     def get_events(self, calendar_id: int) -> dict | bool:
         """Get all events from a calendar by its ID."""
         data = self._handle_request("GET", f"Termine/{calendar_id}")
+        if not data:
+            return False
         return data
 
     @_log_api_result("Termine")
     def add_event(self, data: dict) -> dict | bool:
         """Add an event to a calendar via the API."""
         data = self._validate(data, EventDataModel)
+        if not data:
+            return False
         return self._handle_request("POST", "Termine", data)
